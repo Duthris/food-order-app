d6ceb82e00edc8485d9fea55c0086899b70cad71
Anıl YALIMDEMİR <adem.yalimdemir@std.antalya.edu.tr>
2022-09-22 15:16:32 +0300

    .

--- client/src/pages/Restaurants.tsx
+++ client/src/pages/Restaurants.tsx
@@ -17,25 +17,6 @@ export default function Restaurants() {
         dispatch(getRestaurants());
     }, [dispatch]);
 
-
-    const responsiveOptions = [
-        {
-            breakpoint: '1024px',
-            numVisible: 3,
-            numScroll: 3
-        },
-        {
-            breakpoint: '768px',
-            numVisible: 2,
-            numScroll: 2
-        },
-        {
-            breakpoint: '560px',
-            numVisible: 1,
-            numScroll: 1
-        }
-    ];
-
     return (
         <>
         <div className="container mx-auto items-center h-screen">
